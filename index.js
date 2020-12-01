ab6210da66bbd689e8c7c62e7d2df1b09af30e1a
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-30 22:58:49 -0500

    used .then to write to a file using the generateMarkdown function results

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is a description of you project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions for your project? ',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What usage information should a user know about your project?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are the contribution guidelines?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are the test instuctions',
+        },
+        // {
+        //     type: 'input',
+        //     name: 'license',
+        //     message: 'Please choose a license',
+        // },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+    ]).then((data) => {
+        fs.writeFile('README.md', generateMarkdown(data),(err) => err? console.error(err) : console.log("Success!"))})
 
-const promptUser = () =>
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'What is a description of you project?',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions for your project? ',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What usage information should a user know about your project?',
-    },
-    {
-      type: 'input',
-      name: 'contribution',
-      message: 'What are the contribution guidelines?',
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: 'What are the test instuctions',
-    },
-    // {
-    //     type: 'input',
-    //     name: 'license',
-    //     message: 'Please choose a license',
-    // },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-    },
-  ]);
-
-  
-  promptUser()
-  .then((answers) => console.log(`${answers.title}`));
-  
