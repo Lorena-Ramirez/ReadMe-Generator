3dbb72bea57f5d53b949a486c36af586c5ab289c
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-11-30 22:59:19 -0500

    created template for read me that plugs in data from the user

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,50 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## Description
+  ---
+  ${data.description}
+
+  ## Table of Contents 
+  --- 
+  [Installation](#installation)
+  
+  [Usage](#usage)
+  
+  [Contributing](#contribution)
+
+  [Tests](#test)
+
+  [Questions](#Questions)
+
+  [License](#license)
+  
+  ## Installation
+  ---
+  ${data.installation}
+
+  ## Usage
+  ---
+  ${data.usage}
+
+  ## Contributing
+  ---
+  ${data.contribution}
+
+  ## Test
+  ---
+  ${data.test}
+
+  ## Questions
+  --
+  To reach me with additional questions please contact me with one of the following below.
+  * My github profile - [Github Link](https://github.com/${data.github})
+  * Email - <${data.email}>
+
+ ## License
+  ---
+
+  `;
+}
+
+module.exports = generateMarkdown;
